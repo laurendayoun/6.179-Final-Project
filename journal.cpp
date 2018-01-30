3c52b64b9a6a733736b306d826380f0ea7e5b738
lisayoo <35772343+lisayoo@users.noreply.github.com>
2018-01-30 15:23:44 -0500

    Update journal.cpp

--- journal.cpp
+++ journal.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <map>
 #include <time.h>
+#include <string>
 
+class entry(string title, string time, string text)
+{
+  
+}
 // initially scan password for use -> main function
+int main(int argc, char *argv[])
+{
+  
+}
 // ask for password and check if correct -> function returning bool
+bool checkPass(
 // create new text file to represent a journal entry with timestamp -> 
-//      function updating map
+// function updating map
 // search function by timestamp or contents -> function returning text
