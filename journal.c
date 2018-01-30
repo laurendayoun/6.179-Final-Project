d1eae07a9da8f5a7c9a328175a68d0a148d55e0e
lisayoo <35772343+lisayoo@users.noreply.github.com>
2018-01-30 14:33:19 -0500

    Update journal.c

--- journal.c
+++ journal.c
@@ -1,2 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+// initially scan password for use
+// ask for password and check if correct
+// create new text file to represent a journal entry with timestamp
+// search function by timestamp or contents
