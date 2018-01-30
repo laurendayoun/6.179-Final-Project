cb91f36ebabd7ec7b0e7ac1934ca3ad1e00a4e97
laurendayoun <laurendayoun@gmail.com>
2018-01-30 14:46:08 -0500

    added files

---
+++ journal.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <map>
+#include <time.h>
+
+// initially scan password for use -> main function
+// ask for password and check if correct -> function returning bool
+// create new text file to represent a journal entry with timestamp -> 
+//      function updating map
+// search function by timestamp or contents -> function returning text
