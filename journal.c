5dd801ec8976cc8deda751447e890b01391f8fe1
laurendayoun <laurendayoun@gmail.com>
2018-01-31 01:04:50 -0500

    Delete journal.c

--- journal.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-// initially scan password for use
-// ask for password and check if correct
-// create new text file to represent a journal entry with timestamp
-// search function by timestamp or contents
